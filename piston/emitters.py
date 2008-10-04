a01193005877856c9d8129eb65f6155cce3d7ade
Mads Sulau Joergensen <mads@sulau.dk>
2008-10-04 17:51:12 +0200

    Made a XMLEmitter.

--- piston/emitters.py
+++ piston/emitters.py
@@ -2,7 +2,14 @@ import types, decimal, yaml, copy
 from django.db.models.query import QuerySet
 from django.db.models import Model
 from django.utils import simplejson
+from django.utils.xmlutils import SimplerXMLGenerator
+from django.utils.encoding import smart_unicode
 from django.core.serializers.json import DateTimeAwareJSONEncoder
+
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
 
 class Emitter(object):
     def __init__(self, payload, typemapper):
@@ -104,8 +111,32 @@ class Emitter(object):
             return YAMLEmitter, 'text/plain'
     
 class XMLEmitter(Emitter):
-    pass
-    
+    def _to_xml(self, xml, data):
+        if isinstance(data, (list, tuple)):
+            for item in data:
+                self._to_xml(xml, item)
+        elif isinstance(data, dict):
+            for key, value in data.iteritems():
+                xml.startElement(key, {})
+                self._to_xml(xml, value)
+                xml.endElement(key)
+        elif data:
+            xml.characters(smart_unicode(data))
+
+    def render(self):
+        stream = StringIO.StringIO()
+        
+        xml = SimplerXMLGenerator(stream, "utf-8")
+        xml.startDocument()
+        xml.startElement("response", {})
+        
+        self._to_xml(xml, self.construct())
+        
+        xml.endElement("response")
+        xml.endDocument()
+        
+        return stream.getvalue()
+
 class JSONEmitter(Emitter):
     # TODO: callback functions
     def render(self):
