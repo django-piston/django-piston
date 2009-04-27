73c37b75d100c2c89f2f88110929546299be42ea
Jesper Noehr <jesper@noehr.org>
2009-04-27 11:47:41 +0200

    basic skeleton for a blog app

---
+++ examples/blogserver/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Blogpost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, related_name='posts')
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        return self.title
+
+    
