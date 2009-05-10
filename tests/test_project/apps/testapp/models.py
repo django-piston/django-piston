70568eb08450fccaeca1ad986d20382cae4e1431
Jesper Noehr <jesper@noehr.org>
2009-05-10 18:44:42 +0200

    adding test for incoming json/yaml in tests/

--- tests/test_project/apps/testapp/models.py
+++ tests/test_project/apps/testapp/models.py
@@ -3,4 +3,12 @@ from django.db import models
 class TestModel(models.Model):
     test1 = models.CharField(max_length=1, blank=True, null=True)
     test2 = models.CharField(max_length=1, blank=True, null=True)
-    pass
+    
+class ExpressiveTestModel(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    
+class Comment(models.Model):
+    parent = models.ForeignKey(ExpressiveTestModel, related_name='comments')
+    content = models.TextField()
+    
