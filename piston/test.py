2c634107dd91acf1ffe14964c6b91edc722cb95c
joestump <joe@joestump.net>
2009-08-03 23:42:20 -0600

    Added initial OAuthClient that extends the Django test Client class. Only works with GET right now.

--- piston/test.py
+++ piston/test.py
@@ -4,25 +4,42 @@ import django.test as test
 
 # Piston imports
 from piston import oauth
+from piston.models import Consumer, Token
 
-class OAuthClient(oauth.OAuthClient):
+# 3rd/Python party imports
+import httplib2, urllib
+
+class OAuthClient(client.Client):
     def __init__(self, consumer, token):
         self.token = oauth.OAuthToken(token.key, token.secret)
         self.consumer = oauth.OAuthConsumer(consumer.key, consumer.secret)
         self.signature = oauth.OAuthSignatureMethod_HMAC_SHA1()
 
-    def get(self, path, data={}, headers={}):
-        pass
+        super(OAuthClient, self).__init__()
 
-    def post(self, path, data={}, headers={}, content_type=client.MULTIPART_CONTENT):
-        pass
+    def request(self, **request):
+        # Figure out parameters from request['QUERY_STRING'] and FakePayload
+        if request['REQUEST_METHOD'] in ("POST", "PUT"):
+            print request['wsgi.input'].read()
 
-    def request(self, path, data={}, headers={}):
-        pass
+        url = "http://testserver" + request['PATH_INFO']
+
+        req = oauth.OAuthRequest.from_consumer_and_token(
+            self.consumer, token=self.token, 
+            http_method=request['REQUEST_METHOD'], http_url=url
+        )
+
+        req.sign_request(self.signature, self.consumer, self.token)
+        headers = req.to_header()
+        request['HTTP_AUTHORIZATION'] = headers['Authorization']
+
+        return super(OAuthClient, self).request(**request)
 
 class TestCase(test.TestCase):
     pass
 
 class OAuthTestCase(TestCase):
-    fixtures = ['oauth.json']
+    @property
+    def oauth(self):
+        return OAuthClient(self.consumer, self.token)
 
