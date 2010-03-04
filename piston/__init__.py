494d495e4f5fa820189d51d91c6f465487b4d06b
Ionel Cristian Maries <ionel.maries@lpro.leverpointinc.com>
2010-03-04 16:14:35 +0200

    Added namespacing to the piston package.

---
+++ piston/__init__.py
@@ -0,0 +1,15 @@
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    # don't prevent use of paste if pkg_resources isn't installed
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__) 
+
+try:
+    import modulefinder
+except ImportError:
+    pass
+else:
+    for p in __path__:
+        modulefinder.AddPackagePath(__name__, p)
