4df7d914a4f8b2e922a26e1df2e72940bfcf5ced
Matt Cordes <none@none>
2010-04-01 10:37:16 -0400

    adding ability to return content to be formatted by an emitter and a custom status code. This time were extending the existing rc_factory functionality in piston.utils

--- piston/utils.py
+++ piston/utils.py
@@ -48,7 +48,30 @@ class rc_factory(object):
         except TypeError:
             raise AttributeError(attr)
 
-        return HttpResponse(r, content_type='text/plain', status=c)
+        class HttpResponseWrapper(HttpResponse):
+            """
+            Wrap HttpResponse and make sure that the internal _is_string 
+            flag is updated when the _set_content method (via the content 
+            property) is called
+            """
+            def _set_content(self, content):
+                """
+                Set the _container and _is_string properties based on the 
+                type of the value parameter. This logic is in the construtor
+                for HttpResponse, but doesn't get repeated when setting 
+                HttpResponse.content although this bug report (feature request)
+                suggests that it should: http://code.djangoproject.com/ticket/9403 
+                """
+                if not isinstance(content, basestring) and hasattr(content, '__iter__'):
+                    self._container = content
+                    self._is_string = False
+                else:
+                    self._container = [content]
+                    self._is_string = True
+
+            content = property(HttpResponse._get_content, _set_content)            
+
+        return HttpResponseWrapper(r, content_type='text/plain', status=c)
     
 rc = rc_factory()
     
