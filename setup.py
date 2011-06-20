1150b42999c575a6919a18d0163ab5173dcc8acc
Jorge Eduardo Cardona <jorgeecardona@gmail.com>
2011-06-20 02:20:10 -0500

    Add tests with tox and coverage

--- setup.py
+++ setup.py
@@ -7,23 +7,20 @@ except ImportError:
     import ez_setup
     ez_setup.use_setuptools()
     from setuptools import setup, find_packages
-    
-import os
 
 setup(
-    name = "django-piston",
-    version = "0.2.3rc1",
-    url = 'http://bitbucket.org/jespern/django-piston/wiki/Home',
-	download_url = 'http://bitbucket.org/jespern/django-piston/downloads/',
-    license = 'BSD',
-    description = "Piston is a Django mini-framework creating APIs.",
-    author = 'Jesper Noehr',
-    author_email = 'jesper@noehr.org',
-    packages = find_packages(),
-    namespace_packages = ['piston'],
-    include_package_data = True,
-    zip_safe = False,
-    classifiers = [
+    name="django-piston",
+    version="0.2.3rc1",
+    url='http://bitbucket.org/jespern/django-piston/wiki/Home',
+    download_url='http://bitbucket.org/jespern/django-piston/downloads/',
+    license='BSD',
+    description="Piston is a Django mini-framework creating APIs.",
+    author='Jesper Noehr',
+    author_email='jesper@noehr.org',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
         'Intended Audience :: Developers',
@@ -31,5 +28,5 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
-    ]
+        ],
 )
--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -1,25 +1,6 @@
-import os
-DEBUG = True
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = '/tmp/piston.db'
-INSTALLED_APPS = (
-    'django.contrib.auth', 
-    'django.contrib.contenttypes', 
-    'django.contrib.sessions', 
-    'django.contrib.sites',
-    'piston',
-    'test_project.apps.testapp',
-)
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), 'templates'),
-)
+import django
 
-SITE_ID = 1
-ROOT_URLCONF = 'test_project.urls'
-
-MIDDLEWARE_CLASSES = (
-    'piston.middleware.ConditionalMiddlewareCompatProxy',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'piston.middleware.CommonMiddlewareCompatProxy',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
+if django.VERSION[:2] == (1, 2):
+    from settings12 import *
+else:
+    from settings13 import *
